ef9a052c66e4e28060b578239e63c65218f470ea
pcamposaugusto <paulaocaugusto@gmail.com>
2022-02-10 22:59:41 -0300

    webpack e estrutura react

--- reactjs/01-github-explorer/src/index.jsx
+++ reactjs/01-github-explorer/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { App } from './App';
 
-function App() {
-    return <h1>Hello World</h1>
-}
+render(<h1>Test</h1>, document.getElementById('root'))
