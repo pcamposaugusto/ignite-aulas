9a11d543d96c5e99742bbc1528bcd7b5b3df5a28
pcamposaugusto <paulaocaugusto@gmail.com>
2022-02-14 11:04:39 -0300

    useEffect, listagem, key

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,22 +1,22 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
-
-const repository = {
-    name: "unform",
-    description: "Forms in React",
-    link: "https://github.com/unform.unform"
-}
+import { useState, useEffect } from "react";
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/pcamposaugusto/repos').then(response => response.json()).then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="
         repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>    
         </section>
     );
